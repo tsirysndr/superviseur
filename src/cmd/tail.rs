68abd6e26d914338b7a65d44697bf80ad0e56184
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-03-06 00:15:32 +0300

    feat: implement `log` and `tail` subcommands

--- src/cmd/tail.rs
+++ src/cmd/tail.rs
@@ -1,5 +1,37 @@
 use anyhow::Error;
 
-pub async fn execute_tail(name: &str) -> Result<(), Error> {
-    todo!()
+use crate::api::superviseur::v1alpha1::{
+    control_service_client::ControlServiceClient, logging_service_client::LoggingServiceClient,
+    LoadConfigRequest, TailRequest,
+};
+
+pub async fn execute_tail(name: &str, follow: bool, lines: usize) -> Result<(), Error> {
+    let current_dir = std::env::current_dir()?;
+    let config = std::fs::read_to_string(current_dir.join("Superfile.hcl"))?;
+    let mut client = ControlServiceClient::connect("http://127.0.0.1:5476").await?;
+
+    let request = tonic::Request::new(LoadConfigRequest {
+        config,
+        file_path: current_dir.to_str().unwrap().to_string(),
+    });
+
+    client.load_config(request).await?;
+
+    let mut client = LoggingServiceClient::connect("http://127.0.0.1:5476").await?;
+
+    let request = tonic::Request::new(TailRequest {
+        service: name.to_string(),
+        config_file_path: current_dir.to_str().unwrap().to_string(),
+        follow,
+        lines: lines as u32,
+    });
+
+    let response = client.tail(request).await?;
+    let mut stream = response.into_inner();
+
+    while let Some(message) = stream.message().await? {
+        println!("{}", message.line);
+    }
+
+    Ok(())
 }
--- src/server/logging.rs
+++ src/server/logging.rs
@@ -1,5 +1,7 @@
 use std::{
     collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader, Read},
     sync::{Arc, Mutex},
 };
 
@@ -35,16 +37,143 @@ impl Logging {
 
 #[tonic::async_trait]
 impl LoggingService for Logging {
-    async fn log(&self, _request: Request<LogRequest>) -> Result<Response<LogResponse>, Status> {
-        unimplemented!()
-    }
-
     type TailStream = ReceiverStream<Result<TailResponse, Status>>;
+    type LogStream = ReceiverStream<Result<LogResponse, Status>>;
+
+    async fn log(&self, request: Request<LogRequest>) -> Result<Response<Self::LogStream>, Status> {
+        let request = request.into_inner();
+        let path = request.config_file_path;
+        let name = request.service;
+        let config_map = self.config_map.lock().unwrap();
+
+        if !config_map.contains_key(&path) {
+            return Err(tonic::Status::not_found("Config file not found"));
+        }
+
+        let config = config_map.get(&path).unwrap();
+
+        let service = config
+            .services
+            .iter()
+            .find(|s| s.name == name)
+            .ok_or_else(|| tonic::Status::not_found("Service not found"))?;
+
+        let log_file =
+            File::open(&service.stdout).map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        let (tx, rx) = tokio::sync::mpsc::channel(1024);
+
+        let reader = BufReader::new(log_file);
+
+        for line in reader.lines() {
+            let line = line.map_err(|e| tonic::Status::internal(e.to_string()))?;
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                tx.send(Ok(LogResponse {
+                    line: line.to_string(),
+                }))
+                .await
+                .map_err(|e| tonic::Status::internal(e.to_string()))
+            });
+        }
+
+        if request.follow {
+            let tx = tx.clone();
+            let stdout = service.stdout.clone();
+            tokio::spawn(async move {
+                let mut file = File::open(&stdout).unwrap();
+                let mut buf = [0; 1024];
+                loop {
+                    let n = file.read(&mut buf).unwrap();
+                    if n == 0 {
+                        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+                        continue;
+                    }
+                    let line = String::from_utf8_lossy(&buf[0..n]);
+                    let line = line.trim_end_matches('\n');
+                    tx.send(Ok(LogResponse {
+                        line: line.to_string(),
+                    }))
+                    .await
+                    .unwrap();
+                }
+            });
+        }
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
 
     async fn tail(
         &self,
-        _request: Request<TailRequest>,
+        request: Request<TailRequest>,
     ) -> Result<Response<Self::TailStream>, Status> {
-        unimplemented!()
+        let request = request.into_inner();
+        let path = request.config_file_path;
+        let name = request.service;
+        let config_map = self.config_map.lock().unwrap();
+
+        if !config_map.contains_key(&path) {
+            return Err(tonic::Status::not_found("Config file not found"));
+        }
+
+        let config = config_map.get(&path).unwrap();
+
+        let service = config
+            .services
+            .iter()
+            .find(|s| s.name == name)
+            .ok_or_else(|| tonic::Status::not_found("Service not found"))?;
+
+        let log_file =
+            File::open(&service.stdout).map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        let (tx, rx) = tokio::sync::mpsc::channel(1024);
+
+        let reader = BufReader::new(log_file);
+        let num_lines = request.lines as usize;
+
+        // Read the last `num_lines` lines of the file
+        let mut lines: Vec<String> = reader
+            .lines()
+            .map(|line| line.unwrap())
+            .collect::<Vec<String>>();
+
+        let tail_lines = lines.split_off(lines.len().saturating_sub(num_lines));
+
+        for line in tail_lines {
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                tx.send(Ok(TailResponse {
+                    line: line.to_string(),
+                }))
+                .await
+                .map_err(|e| tonic::Status::internal(e.to_string()))
+            });
+        }
+
+        if request.follow {
+            let tx = tx.clone();
+            let stdout = service.stdout.clone();
+            tokio::spawn(async move {
+                let mut file = File::open(&stdout).unwrap();
+                let mut buf = [0; 1024];
+                loop {
+                    let n = file.read(&mut buf).unwrap();
+                    if n == 0 {
+                        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+                        continue;
+                    }
+                    let line = String::from_utf8_lossy(&buf[0..n]);
+                    let line = line.trim_end_matches('\n');
+                    tx.send(Ok(TailResponse {
+                        line: line.to_string(),
+                    }))
+                    .await
+                    .unwrap();
+                }
+            });
+        }
+
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 }
