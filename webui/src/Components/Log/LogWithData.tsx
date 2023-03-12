7e9319e71a2603bd641a2a4f987830d57e7345fe
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-03-12 23:04:26 +0300

    feat(webui): add dot pattern background

--- webui/src/Components/Log/LogWithData.tsx
+++ webui/src/Components/Log/LogWithData.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useGetLogsQuery } from "../../Hooks/GraphQL";
 import { lines } from "../../Mocks/Lines";
 import Log from "./Log";
 
--- webui/src/Providers/index.tsx
+++ webui/src/Providers/index.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import ThemeProvider from "./ThemeProvider";
+import { MockedProvider } from "@apollo/client/testing";
 
 export type ProvidersProps = {
   children: React.ReactNode;
 };
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <MockedProvider mocks={[]} addTypename={false}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </MockedProvider>
+  );
 };
 
 export default Providers;
