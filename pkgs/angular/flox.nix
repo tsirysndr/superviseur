3cb9225d7ba5c2345c36c4615ef76239ad800805
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-03-24 01:14:24 +0300

    add angular to examples

---
+++ pkgs/angular/flox.nix
@@ -0,0 +1,10 @@
+{
+  # flox environment configuration
+  #
+  # flox.nix options: https://floxdev.com/docs/reference/flox-nix-config
+  # Getting started with flox: https://floxdev.com/docs
+  # Get help: https://discourse.floxdev.com
+  #
+  # Happy hacking!
+  packages.nixpkgs-flox.nodejs-17_x = {};
+}
