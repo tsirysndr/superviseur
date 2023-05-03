3e1fbfff668bf017cbb08ada0f08e1f2d222ea3b
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-05-03 22:37:01 +0300

    sdk: remove unused deno files

--- sdk/deno/main_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.184.0/testing/asserts.ts";
-import { add } from "./main.ts";
-
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
-});
