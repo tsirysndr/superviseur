323b7337ea57037590516d0a477dac1c2739d9ef
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-05-19 23:38:50 +0300

    feat(website): add code examples

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,45 +1,10 @@
 import React from "react";
 import clsx from "clsx";
+import "prismjs/themes/prism-dark.css";
+import "prismjs/components/prism-hcl";
+import "prismjs/components/prism-typescript";
 import styles from "./styles.module.css";
-
-type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: "Easy to Use",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: "Focus on What Matters",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: "Powered by React",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
+import CodeExample from "./CodeExample.mdx";
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
@@ -59,7 +24,9 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row"></div>
+        <div className="row">
+          <CodeExample />
+        </div>
       </div>
     </section>
   );
