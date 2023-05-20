d8471f9d8dbca80f5ebfd6fb43cff0ada84a103a
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-05-20 12:46:50 +0300

    feat(website): show supported runtimes

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -5,6 +5,7 @@ import "prismjs/components/prism-hcl";
 import "prismjs/components/prism-typescript";
 import styles from "./styles.module.css";
 import CodeExample from "./CodeExample.mdx";
+import SupportedPlugins from "./SupportedPlugins.mdx";
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
@@ -22,52 +23,97 @@ function Feature({ title, Svg, description }: FeatureItem) {
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
-      <div
-        className="container"
-        style={{ display: "flex", flexDirection: "row" }}
-      >
-        <div className="row" style={{ flex: 1 }}>
-          <CodeExample />
-        </div>
+    <>
+      <section className={styles.features}>
         <div
-          style={{
-            display: "flex",
-            alignItems: "start",
-            flex: 1,
-            paddingLeft: 40,
-            marginTop: 100,
-          }}
+          className="container"
+          style={{ display: "flex", flexDirection: "row" }}
         >
-          <div style={{ fontSize: "22px" }}>
-            Define and run{" "}
-            <span style={{ color: "#00ffed", fontFamily: "RockfordSans Bold" }}>
-              multi-service
-            </span>{" "}
-            applications on{" "}
-            <span style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}>
-              isolated
-            </span>{" "}
-            environments with{" "}
-            <span style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}>
-              Nix
-            </span>{" "}
-            or{" "}
-            <span style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}>
-              Docker
-            </span>{" "}
-            using{" "}
-            <span style={{ color: "#8900ff", fontFamily: "RockfordSans Bold" }}>
-              HCL
-            </span>{" "}
-            or{" "}
-            <span style={{ color: "#8900ff", fontFamily: "RockfordSans Bold" }}>
-              any language
-            </span>{" "}
-            you already know.
+          <div style={{ flex: 1 }}>
+            <CodeExample />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "start",
+              flex: 1,
+              paddingLeft: 40,
+              marginTop: 100,
+            }}
+          >
+            <div style={{ fontSize: "22px" }}>
+              Define and run{" "}
+              <span
+                style={{ color: "#00ffed", fontFamily: "RockfordSans Bold" }}
+              >
+                multi-service
+              </span>{" "}
+              applications on{" "}
+              <span
+                style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}
+              >
+                isolated
+              </span>{" "}
+              environments with{" "}
+              <span
+                style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}
+              >
+                Nix
+              </span>{" "}
+              or{" "}
+              <span
+                style={{ color: "#f800ff", fontFamily: "RockfordSans Bold" }}
+              >
+                Docker
+              </span>{" "}
+              using{" "}
+              <span
+                style={{ color: "#8900ff", fontFamily: "RockfordSans Bold" }}
+              >
+                HCL
+              </span>{" "}
+              or{" "}
+              <span
+                style={{ color: "#8900ff", fontFamily: "RockfordSans Bold" }}
+              >
+                any language
+              </span>{" "}
+              you already know.
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className={styles.features}>
+        <div
+          className="container"
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "start",
+              flex: 1,
+              paddingRight: 100,
+              marginTop: 100,
+            }}
+          >
+            <div style={{ fontSize: "22px" }}>
+              Support many different environments and runtimes, including{" "}
+              <span style={{ color: "#3fe", fontFamily: "RockfordSans Bold" }}>
+                Docker, Nix, Spin, WebAssembly
+              </span>{" "}
+              and{" "}
+              <span style={{ color: "#3fe", fontFamily: "RockfordSans Bold" }}>
+                more
+              </span>
+              .
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <SupportedPlugins />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
